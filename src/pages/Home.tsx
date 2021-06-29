9171cd18eff607da3e21c5630f2725c837bf9180
Eduardo Augusto <eduardopetreca@gmail.com>
2021-06-29 19:31:48 -0300

    refactor code to typeScript format

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,15 +7,26 @@ import {
   Platform,
   FlatList,
 } from 'react-native';
+
 import {Button} from '../components/Button';
 import {SkillCard} from '../components/SkillCard';
+
+interface SkillData {
+  id: String;
+  name: String;
+}
+
 export function Home() {
-  const [gretting, setGretting] = useState('');
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
+  const [gretting, setGretting] = useState('');
 
   function handleAddNewSkill() {
-    setMySkills(oldState => [...oldState, newSkill]);
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill,
+    };
+    setMySkills(oldState => [...oldState, data]);
   }
 
   useEffect(() => {
@@ -43,8 +54,8 @@ export function Home() {
       <Text style={[styles.title, {marginVertical: 50}]}>My Skills</Text>
       <FlatList
         data={mySkills}
-        keyExtractor={item => item}
-        renderItem={({item}) => <SkillCard skill={item} />}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => <SkillCard skill={item.name} />}
       />
     </View>
   );
